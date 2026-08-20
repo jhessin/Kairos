ed4e21b70e7532747c3aa7536674d1d9f1593bde
Jim Hessin <jhessin@gmail.com>
2026-08-20 11:56:49 -0600

    End of Lesson 03

---
+++ systems/godot_camera_system.gd
@@ -0,0 +1,27 @@
+class_name GodotCameraSystem
+extends System
+
+
+func deps() -> Dictionary[int, Array]:
+	return { Runs.After: [CameraSystem] }
+
+
+func query() -> QueryBuilder:
+	return q.with_all([C_CameraState, C_GodotCamera])
+
+
+func process(entities: Array[Entity], _components: Array, _delta: float) -> void:
+	for entity in entities:
+		var camera_state := entity.get_component(C_CameraState) as C_CameraState
+		var godot_camera := entity.get_component(C_GodotCamera) as C_GodotCamera
+
+		var rig := godot_camera.rig
+		var spring_arm := godot_camera.spring_arm
+
+		if not is_instance_valid(rig) or not is_instance_valid(spring_arm):
+			continue
+
+		rig.rotation_degrees.x = camera_state.pitch
+		rig.rotation_degrees.y = camera_state.yaw
+
+		spring_arm.spring_length = camera_state.distance
--- systems/player_input_system.gd
+++ systems/player_input_system.gd
@@ -10,6 +10,12 @@ func query() -> QueryBuilder:
 
 
 func process(entities: Array[Entity], _components: Array, _delta: float) -> void:
+	if Input.is_action_just_pressed('ui_cancel'):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
 	for entity in entities:
 		var player := entity.get_component(C_Player) as C_Player
 		var intent := entity.get_component(C_MovementIntent) as C_MovementIntent
@@ -24,6 +30,7 @@ func process(entities: Array[Entity], _components: Array, _delta: float) -> void
 			intent.direction = Vector3.ZERO
 			continue
 
+		# Get the user input
 		var input_2d := Input.get_vector('move_left', 'move_right', 'move_backward', 'move_forward')
 
 		var camera_forward := -camera.global_transform.basis.z
