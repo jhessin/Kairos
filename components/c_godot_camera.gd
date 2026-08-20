62cf0bfe340a5ee1e7b9b211e5013085796581e2
Jim Hessin <jhessin@gmail.com>
2026-08-20 11:48:27 -0600

    End of Lesson 2

---
+++ components/c_godot_camera.gd
@@ -0,0 +1,4 @@
+class_name C_GodotCamera
+extends Component
+
+var camera: Camera3D
--- entities/player_entity.gd
+++ entities/player_entity.gd
@@ -7,15 +7,26 @@ extends Entity
 
 
 func define_components() -> Array:
-	return [C_Player.new(), C_MovementIntent.new(), C_Movement.new(), C_GodotCharacterBody.new()]
+	return [
+		C_Player.new(),
+		C_MovementIntent.new(),
+		C_Movement.new(),
+		C_GodotCharacterBody.new(),
+		C_GroundState.new(),
+		C_GodotCamera.new(),
+	]
 
 
 # Remember Entities are just containers and glue code
 func on_ready() -> void:
 	var godot_body := get_component(C_GodotCharacterBody) as C_GodotCharacterBody
+	var godot_camera := get_component(C_GodotCamera) as C_GodotCamera
 
 	if godot_body:
 		godot_body.body = character_body
+
+	if godot_camera:
+		godot_camera.camera = camera
 
 	if camera:
 		camera.current = true
