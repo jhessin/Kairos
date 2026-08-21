c896dac03ab7351d6ba9741108c8feb21da5df9f
Jim Hessin <jhessin@gmail.com>
2026-08-21 09:19:36 -0600

    add controller camera controls.

--- systems/camera_system.gd
+++ systems/camera_system.gd
@@ -6,7 +6,7 @@ func query() -> QueryBuilder:
 	return q.with_all([C_Player, C_CameraState])
 
 
-func process(entities: Array[Entity], _components: Array, _delta: float) -> void:
+func process(entities: Array[Entity], _components: Array, delta: float) -> void:
 	for entity in entities:
 		var player := entity.get_component(C_Player) as C_Player
 		var camera_state := entity.get_component(C_CameraState) as C_CameraState
@@ -14,22 +14,32 @@ func process(entities: Array[Entity], _components: Array, _delta: float) -> void
 		if not player.is_local:
 			continue
 
-		# Only act if the mouse is captured
-		if Input.mouse_mode != Input.MOUSE_MODE_CAPTURED:
-			continue
+		# Mouse Camera Control
+		if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+			# Get the input from the user.
+			var mouse_velocity := Input.get_last_mouse_velocity()
 
-		# Get the input from the user.
-		var mouse_velocity := Input.get_last_mouse_velocity()
+			# Set the input of the ECS component
+			camera_state.yaw -= mouse_velocity.x * camera_state.mouse_sensitivity
+			camera_state.pitch -= mouse_velocity.y * camera_state.mouse_sensitivity
 
+		# Controller camera control.
+		var camera_input := Input.get_vector(
+			'camera_left',
+			'camera_right',
+			'camera_up',
+			'camera_down',
+		)
+
+		camera_state.yaw -= (camera_input.x * camera_state.controller_sensitivity * delta)
+		camera_state.pitch -= (camera_input.y * camera_state.controller_sensitivity * delta)
+
+		# Mouse wheel zoom
 		if Input.is_action_just_pressed('camera_zoom_in'):
 			camera_state.distance -= 0.5
 
 		if Input.is_action_just_pressed('camera_zoom_out'):
 			camera_state.distance += 0.5
-
-		# Set the input of the ECS component
-		camera_state.yaw -= mouse_velocity.x * camera_state.sensitivity
-		camera_state.pitch -= mouse_velocity.y * camera_state.sensitivity
 
 		# Clamp values appropriately
 		camera_state.pitch = clamp(
