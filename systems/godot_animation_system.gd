aded2e259e22a761a3a765a7d2e3bda545112a6b
Jim Hessin <jhessin@gmail.com>
2026-08-21 18:26:15 -0600

    end of lesson 6

---
+++ systems/godot_animation_system.gd
@@ -0,0 +1,43 @@
+class_name GodotAnimationSystem
+extends System
+
+
+func deps() -> Dictionary[int, Array]:
+	return { Runs.After: [AnimationStateSystem] }
+
+
+func query() -> QueryBuilder:
+	return q.with_all([C_AnimationState, C_GodotAnimation])
+
+
+func process(entities: Array[Entity], _components: Array, _delta: float) -> void:
+	for entity in entities:
+		var animation_state := entity.get_component(C_AnimationState) as C_AnimationState
+		var godot_animation := entity.get_component(C_GodotAnimation) as C_GodotAnimation
+
+		var animation_player := godot_animation.animation_player
+
+		if not is_instance_valid(animation_player):
+			continue
+
+		if animation_state.current == animation_state.previous:
+			continue
+
+		var animation_name := ""
+
+		match animation_state.current:
+			C_AnimationState.State.IDLE:
+				animation_name = 'idle'
+			C_AnimationState.State.WALK:
+				animation_name = 'walk'
+			C_AnimationState.State.RUN:
+				animation_name = 'run'
+
+		if animation_name == '':
+			continue
+
+		if (
+			animation_player.has_animation(animation_name)
+			# and animation_player.current_animation != animation_name
+		):
+			animation_player.play(animation_name)
