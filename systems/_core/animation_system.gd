03804eb1f2f0eafabfd2be491a4c55eec130ff9e
Jim Hessin <jhessin@gmail.com>
2026-09-05 17:31:43 -0600

    refactor: moved FacingSystem into AnimationSystem

--- systems/_core/animation_system.gd
+++ systems/_core/animation_system.gd
@@ -3,11 +3,18 @@ extends System
 
 
 func deps() -> Dictionary[int, Array]:
-	return { Runs.After: [MovementSystem] }
+	return {
+		Runs.After: [MovementSystem],
+		Runs.Before: [
+			GodotSystem3d,
+			# GodotSystem2d,
+		],
+	}
 
 
 func sub_systems() -> Array[Array]:
 	return [
+		[q.with_all([C_Facing, C_Rotation]), Facing.process],
 		[q.with_all([C_Movement, C_MovementMode, C_AnimationState]), AnimationState.process],
 		[q.with_all([C_AnimationState, C_GodotAnimation]), GodotAnimation.process],
 	]
---
+++ systems/animation/facing.gd
@@ -0,0 +1,26 @@
+class_name Facing
+extends RefCounted
+
+
+static func process(entities: Array[Entity], _components: Array, delta: float) -> void:
+	for entity in entities:
+		var facing := entity.get_component(C_Facing) as C_Facing
+		var rotation := entity.get_component(C_Rotation) as C_Rotation
+
+		var target_direction := facing.direction
+		target_direction.y = 0.0
+
+		if target_direction.length_squared() < 0.001:
+			continue
+
+		target_direction = target_direction.normalized()
+
+		var target_yaw := atan2(target_direction.x, target_direction.z)
+
+		var difference := angle_difference(rotation.yaw, target_yaw)
+
+		var max_rotation := facing.rotation_speed * delta
+
+		difference = clamp(difference, -max_rotation, max_rotation)
+
+		rotation.yaw += difference
